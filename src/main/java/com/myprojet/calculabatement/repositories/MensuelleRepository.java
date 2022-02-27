909e65ea8be5ec36686040750ed4eef769fd084e
Christine Duarte <christine_dos_santos@hotmail.fr>
2022-02-27 18:58:35 +0100

    creation Mensuelle models and MensuelleRepository

---
+++ src/main/java/com/myprojet/calculabatement/repositories/MensuelleRepository.java
@@ -0,0 +1,7 @@
+package com.myprojet.calculabatement.repositories;
+
+import com.myprojet.calculabatement.models.Mensuelle;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MensuelleRepository extends CrudRepository<Mensuelle, Integer> {
+}
