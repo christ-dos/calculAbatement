b58b668bada3d1b7889f676265173527050527ed
Christine Duarte <christine_dos_santos@hotmail.fr>
2022-03-02 15:10:51 +0100

    implementation tests in ChildRepositoryTest for methodes deleteById, findById, save, existById

--- src/test/java/com/myprojet/calculabatement/repositories/ChildRepositoryTest.java
+++ src/test/java/com/myprojet/calculabatement/repositories/ChildRepositoryTest.java
@@ -9,8 +9,9 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @SpringBootTest
@@ -34,6 +35,75 @@ public class ChildRepositoryTest {
         assertEquals(2, childrenByUserEmail.size());
     }
 
+    @Test
+    public void saveChildTest_thenReturnChildAdded() {
+        //GIVEN
+        Child childToSave = new Child(1, "Benoit", "Evan", "14/12/2014", "15/03/2020", "christine@email.fr");
+        //WHEN
+        Child childResult = childRepositoryTest.save(childToSave);
+        //THEN
+        assertEquals(1, childResult.getId());
+        assertEquals("Benoit", childResult.getLastname());
+        assertEquals("Evan", childResult.getFirstname());
+    }
 
+    @Test
+    public void existByIdTest_whenChildExist_thenReturnTrue(){
+        //GIVEN
+        Child childTest = new Child(1, "Benoit", "Evan", "14/12/2014", "15/03/2020", "christine@email.fr");
+        //WHEN
+        Child childResult = childRepositoryTest.save(childTest);
+        Boolean childExist = childRepositoryTest.existsById(1);
+        //THEN
+        assertTrue(childExist);
+    }
 
+    @Test
+    public void existByIdTest_whenChildNotExist_thenReturnFalse(){
+        //GIVEN
+        Child childTest = new Child(1, "Benoit", "Evan", "14/12/2014", "15/03/2020", "christine@email.fr");
+        //WHEN
+        Boolean childExist = childRepositoryTest.existsById(1);
+        //THEN
+        //verify that child is not present in DB
+        assertFalse(childExist);
+    }
+
+    @Test
+    public void findByIdTest_whenChildExist_thenReturnChildFound(){
+        //GIVEN
+        Child childTest= new Child(1, "Benoit", "Evan", "14/12/2014", "15/03/2020", "christine@email.fr");
+        //WHEN
+        Child childResult = childRepositoryTest.save(childTest);
+        Optional<Child> childFind = childRepositoryTest.findById(1);
+        //THEN
+        //verify that child is present in DB
+        assertTrue(childFind.isPresent());
+    }
+
+    @Test
+    public void findByIdTest_whenChildNotExist_thenReturnEmptyOptinal(){
+        //GIVEN
+        Child childTest = new Child(1, "Benoit", "Evan", "14/12/2014", "15/03/2020", "christine@email.fr");
+        //WHEN
+        Optional<Child> childFind = childRepositoryTest.findById(1);
+        //THEN
+        //verify that child is not present in DB
+        assertFalse(childFind.isPresent());
+    }
+
+    @Test
+    public void deleteByIdTest_whenChildExist() {
+        Child childTest = new Child(1, "Benoit", "Evan", "14/12/2014", "15/03/2020", "christine@email.fr");
+        //WHEN
+        Child childResult = childRepositoryTest.save(childTest);
+        Optional<Child> childFoundBeforeDeletion = childRepositoryTest.findById(1);
+        childRepositoryTest.deleteById(1);
+        Optional<Child> childFoundAfterDeletion = childRepositoryTest.findById(1);
+        //THEN
+        //verify that child exists in DB after adding
+        assertTrue(childFoundBeforeDeletion.isPresent());
+        //verify that child not exists in DB after deletion
+        assertFalse(childFoundAfterDeletion.isPresent());
+    }
 }
