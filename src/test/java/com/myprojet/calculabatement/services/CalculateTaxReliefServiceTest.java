3827f52657c1af8996873cf72c3166e05f81ce9a
Christine Duarte <christine_dos_santos@hotmail.fr>
2022-03-13 00:20:50 +0100

    implementation tests for method calculateTaxReliefByChild only with hoursWorked

--- src/test/java/com/myprojet/calculabatement/services/CalculateTaxReliefServiceTest.java
+++ src/test/java/com/myprojet/calculabatement/services/CalculateTaxReliefServiceTest.java
@@ -41,7 +41,24 @@ public class CalculateTaxReliefServiceTest {
                 new Monthly(2, Month.FEBRUARY, "2022", 650D, 20, 20, 20, 0D, 1),
                 new Monthly(3, Month.MARCH, "2022", 650D, 20, 20, 20, 0D, 1)
         );
-        double calculateTaxRelief = Precision.round(20 * 3  * (10D * 3), 2); //1800.0
+        double calculateTaxRelief = Precision.round(20 * 3 * (10D * 3), 2); //1800.0
+        //WHEN
+        when(monthlyRepositoryMock.findMonthlyByYear(anyString())).thenReturn(monthliesByYear);
+        Double taxReliefResult = calculateTaxReliefServiceTest.calculateTaxReliefByChild(10D, 0.00, Month.FEBRUARY, "2022", 1);
+        //THEN
+        assertEquals(calculateTaxRelief, taxReliefResult);
+        verify(monthlyRepositoryMock, times(1)).findMonthlyByYear(anyString());
+    }
+
+    @Test
+    public void calculateTaxReliefByChildTest_whenRateSmicUpWareOneTimeAndRateSmic2IsEqualZeroWithoutDaysWorked_thenReturnTaxReliefForCompleteYear() {
+        //GIVEN
+        List<Monthly> monthliesByYear = Arrays.asList(
+                new Monthly(1, Month.JANUARY, "2022", 650D, 20, 20, 0, 50.0, 1),
+                new Monthly(2, Month.FEBRUARY, "2022", 650D, 20, 20, 0, 50.0, 1),
+                new Monthly(3, Month.MARCH, "2022", 650D, 20, 20, 0, 50.0, 1)
+        );
+        double calculateTaxRelief = Precision.round((0 + (Math.ceil(50D * 3 / 8))) * (10D * 3), 2);//570.0
         //WHEN
         when(monthlyRepositoryMock.findMonthlyByYear(anyString())).thenReturn(monthliesByYear);
         Double taxReliefResult = calculateTaxReliefServiceTest.calculateTaxReliefByChild(10D, 0.00, Month.FEBRUARY, "2022", 1);
@@ -84,6 +101,23 @@ public class CalculateTaxReliefServiceTest {
     }
 
     @Test
+    public void calculateTaxReliefByChildTest_whenRateSmicUpWareTwoTimesAndWithDaysWorked_thenReturnTaxReliefCalculatedForTwoDifferentPeriods() {
+        List<Monthly> monthliesByYear = Arrays.asList(
+                new Monthly(1, Month.JANUARY, "2022", 650D, 20, 20, 0, 50.0, 1),
+                new Monthly(2, Month.FEBRUARY, "2022", 650D, 20, 20, 0, 50.0, 1),
+                new Monthly(3, Month.JUNE, "2022", 650D, 20, 20, 0, 50.0, 1)
+        );
+        double calculateTaxReliefForTwoPeriod = Precision.round((0 * 2 + (Math.ceil(50D * 2 / 8))) * (10D * 3) + (0 + (Math.ceil(50D / 8))) * (10.10 * 3), 2);//602.10
+        //WHEN
+        when(monthlyRepositoryMock.findMonthlyByYear(anyString())).thenReturn(monthliesByYear);
+        Double taxReliefResult = calculateTaxReliefServiceTest.calculateTaxReliefByChild(10D, 10.10, Month.JUNE, "2022", 1);
+        //THEN
+        assertEquals(calculateTaxReliefForTwoPeriod, taxReliefResult);
+        verify(monthlyRepositoryMock, times(1)).findMonthlyByYear(anyString());
+    }
+
+
+    @Test
     public void calculateTaxReliefByChildTest_whenRateSmicUpWareTwoTimesAndWithHoursWorked_thenReturnTaxReliefCalculatedForTwoDifferentPeriods() {
         List<Monthly> monthliesByYear = Arrays.asList(
                 new Monthly(1, Month.JANUARY, "2022", 650D, 20, 20, 20, 10.0, 1),
@@ -124,7 +158,7 @@ public class CalculateTaxReliefServiceTest {
                 new Monthly(3, Month.DECEMBER, "2022", 650D, 20, 20, 20, 10.0, 1)
         );
         //  0 car il n'y a rien avant Janvier
-        double calculateTaxReliefForTwoPeriod = Precision.round((20 * 2 + (Math.ceil(10D * 2 / 8))) * (10D * 3) + (20  + (Math.ceil(10D / 8))) * (10.10 * 3), 2);//1956.6
+        double calculateTaxReliefForTwoPeriod = Precision.round((20 * 2 + (Math.ceil(10D * 2 / 8))) * (10D * 3) + (20 + (Math.ceil(10D / 8))) * (10.10 * 3), 2);//1956.6
         //WHEN
         when(monthlyRepositoryMock.findMonthlyByYear(anyString())).thenReturn(monthliesByYear);
         Double taxReliefResult = calculateTaxReliefServiceTest.calculateTaxReliefByChild(10D, 10.10, Month.DECEMBER, "2022", 1);
@@ -132,6 +166,7 @@ public class CalculateTaxReliefServiceTest {
         assertEquals(calculateTaxReliefForTwoPeriod, taxReliefResult);
         verify(monthlyRepositoryMock, times(1)).findMonthlyByYear(anyString());
     }
+
 
     @Test
     public void calculateTaxReliefByChildTest_whenNoEntryWasFoundForYear2022_thenThrowMonthlyNotFoundException() {
