2164fa121c251cdf7045478d6a68aa07b9b7fd43
Christine Duarte <christine_dos_santos@hotmail.fr>
2022-04-03 16:26:03 +0200

    implementation endpoints in restController /add, /update, /find/{id} and /delete/{id}

--- src/main/java/com/myprojet/calculabatement/restControllers/ChildRestController.java
+++ src/main/java/com/myprojet/calculabatement/restControllers/ChildRestController.java
@@ -6,14 +6,13 @@ import com.myprojet.calculabatement.services.TaxableSalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequestMapping("/child")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ChildRestController {
     @Autowired
@@ -22,15 +21,39 @@ public class ChildRestController {
     private TaxableSalaryService taxableSalaryService;
 
 
-    @GetMapping("/child/all")
+    @GetMapping("/all")
     public ResponseEntity<Iterable<Child>> getAllChildren(){
         Iterable<Child> children = childService.getChildrenByUserEmail();
         return new ResponseEntity<>(children, HttpStatus.OK);
     }
 
-    @GetMapping("/child/taxablesalary")
-    public ResponseEntity<List<Double>>getAnnualTaxableSalary(int childId, String year){
-        List<Double> taxableSalaries = Collections.singletonList(taxableSalaryService.getSumTaxableSalaryByChildAndByYear(year, childId));
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Child> getChildById(@PathVariable("id") int childId){
+        Child child = childService.getChildById(childId);
+        return new ResponseEntity<>(child, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Child> addChild(@RequestBody Child child){
+        Child newChild = childService.addChild(child);
+        return new ResponseEntity<>(newChild, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Child> updateChild(@RequestBody Child child){
+        Child updateChild = childService.updateChild(child);
+        return new ResponseEntity<>(updateChild, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteChild(@PathVariable("id") int childId){
+        childService.deleteChildById(childId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/taxablesalary")
+    public ResponseEntity<List<Double>>getAnnualTaxableSalaryAllChildren(List<Integer>childIds, String year){
+        //List<Double> taxableSalaries = Collections.singletonList(taxableSalaryService.getSumTaxableSalaryByChildAndByYear(year, ));
         return null;
     }
 }
